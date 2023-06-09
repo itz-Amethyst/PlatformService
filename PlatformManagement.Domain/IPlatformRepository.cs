e7de6b885f47ef2c3083acf1ccdd60425ab96c43
itz-amethyst <pransermi@gmail.com>
2023-06-09 21:59:58 +0430

    feat: Added IPlatformRepository

---
+++ PlatformManagement.Domain/IPlatformRepository.cs
@@ -0,0 +1,10 @@
+﻿using _0_Framework.Domain;
+using PlatformManagement.Application.Contracts.Platform;
+
+namespace PlatformManagement.Domain
+{
+    public interface IPlatformRepository : IRepository<int , Platform>
+    {
+        void CreatePlatform(CreatePlatform command);
+    }
+}
