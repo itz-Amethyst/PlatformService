976a5d9b6cde60daf62924871e946202d49e66cf
itz-amethyst <pransermi@gmail.com>
2023-06-09 21:59:35 +0430

    feat: Added CreatePlatform DTO

---
+++ PlatformManagement.Application.Contracts/Platform/CreatePlatform.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using _0_Framework.Application.Common;
+
+namespace PlatformManagement.Application.Contracts.Platform
+{
+    public class CreatePlatform
+    {
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        public string Publisher { get; set; }
+
+        [Required(ErrorMessage = ValidationMessages.IsRequired)]
+        public string Cost { get; set; }
+    }
+}
