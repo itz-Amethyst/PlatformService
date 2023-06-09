61cbf16f5eee261288af5fef786e18ec4f1346e9
itz-amethyst <pransermi@gmail.com>
2023-06-09 21:59:22 +0430

    feat: Added ValidationMessages Class

---
+++ 0_Framework/Application/Common/ValidationMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace _0_Framework.Application.Common
+{
+    public class ValidationMessages
+    {
+        public const string IsRequired = "این مقدار نمیتواند خالی باشد";
+        public const string NotValid = "مقدار وارد شده صحیح نمیباشد";
+        public const string MaxFileSize = "فایل حجیم تر ازحد مجاز است";
+        public const string InvalidFileFormat = "فرمت فایل مجاز نیست لطفا فقط (jpg , jpeg , png) انتخاب کنید";
+        public const string EmailIsNotValid = "ایمل وارد شده معتبر نمیباشد";
+        public const string PasswordsDoNotMatch = "رمز عبور و تکرار آن یکسان نیست";
+    }
+}
