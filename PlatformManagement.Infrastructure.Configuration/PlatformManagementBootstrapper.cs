add50dcaa74667e9a319474573d51b4c6b599939
itz-amethyst <pransermi@gmail.com>
2023-06-09 23:18:57 +0430

    feat: Configured in bootstrapper

--- PlatformManagement.Infrastructure.Configuration/PlatformManagementBootstrapper.cs
+++ PlatformManagement.Infrastructure.Configuration/PlatformManagementBootstrapper.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using PlatformManagement.Domain;
 using PlatformManagement.Infrastructure.EFCore.Context;
+using PlatformManagement.Infrastructure.EFCore.Repositories;
 
 namespace PlatformManagement.Infrastructure.Configuration
 {
@@ -8,6 +10,13 @@ namespace PlatformManagement.Infrastructure.Configuration
     {
         public static void Configure(IServiceCollection services)
         {
+
+            #region Repository
+
+            services.AddTransient<IPlatformRepository, PlatformRepository>();
+
+            #endregion
+
             services.AddDbContext<PlatformContext>(x=>x.UseInMemoryDatabase("InMem"));
         }
     }
