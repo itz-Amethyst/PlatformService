0a0a44a48cd2343988e152ac0924350bca34e3d6
itz-amethyst <pransermi@gmail.com>
2023-06-10 18:32:34 +0430

    feat: Added PlatformViewModel

---
+++ PlatformManagement.Application.Contracts/Platform/PlatformViewModel.cs
@@ -0,0 +1,18 @@
+﻿using _0_Framework.Application.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlatformManagement.Application.Contracts.Platform
+{
+    public class PlatformViewModel
+    {
+        public int Id { get; set; }
+
+        public string CreationDate { get; set; }
+
+        public string Name { get; set; }
+
+        public string Publisher { get; set; }
+
+        public string Cost { get; set; }
+    }
+}
