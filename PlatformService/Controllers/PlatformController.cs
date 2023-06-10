a91be521d2d4bf923d7b03512cf8eabbfcb812d0
itz-amethyst <pransermi@gmail.com>
2023-06-10 18:32:51 +0430

    feat: Added Basics of platformController

---
+++ PlatformService/Controllers/PlatformController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlatformManagement.Application.Contracts.Platform;
+using PlatformManagement.Domain;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly IPlatformRepository _platformRepository;
+
+        public PlatformController(IPlatformRepository platformRepository, ILogger logger)
+        {
+            _platformRepository = platformRepository;
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformViewModel>> GetPlatforms()
+        {
+            _logger.LogWarning("--> Getting Platforms <--");
+            //Console.WriteLine("--> Getting Platforms");
+
+            var platformItem = _platformRepository.GetAllPlatforms();
+        }
+    }
+}
