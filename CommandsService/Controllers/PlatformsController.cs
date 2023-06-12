9366751efbc1582721a7de59f57d52450f597b30
itz-amethyst <pransermi@gmail.com>
2023-06-12 12:08:11 +0430

    feat: Added new Controller to command

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -2,10 +2,22 @@
 
 namespace CommandsService.Controllers
 {
-    [Route("api/[controller]")]
+
+    [Route("api/c/[controller]")]
     [ApiController]
     public class PlatformsController : ControllerBase
     {
+        public PlatformsController()
+        {
+            
+        }
 
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound Post # Command  Service <--");
+
+            return Ok("Inbound test of from Platforms Controller");
+        }
     }
 }
