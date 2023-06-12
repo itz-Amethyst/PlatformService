d3ce0bc9c560b648d80d37be63d4c17f92cbde92
itz-amethyst <pransermi@gmail.com>
2023-06-12 12:21:56 +0430

    feat: Added SendPlatformToCommand http method

--- PlatformService/SyncDataServices/http/HttpCommandDataClient.cs
+++ PlatformService/SyncDataServices/http/HttpCommandDataClient.cs
@@ -1,7 +1,42 @@
-﻿namespace PlatformService.SyncDataServices.http
+﻿using System.Text;
+using System.Text.Json;
+using PlatformManagement.Application.Contracts.Platform;
+
+namespace PlatformService.SyncDataServices.http
 {
-    public class HttpCommandDataClient
+    public class HttpCommandDataClient : ICommandDataClient
     {
-        public HttpCommandDataClient() { }
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+        public HttpCommandDataClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+        public async Task SendPlatformToCommand(PlatformViewModel command)
+        {
+            var httpContent = new StringContent(
+                JsonSerializer.Serialize(command),
+                Encoding.UTF8,
+            "application/json");
+
+            //? Goes to appsettings.json and pick CommandService url
+
+            var response = await _httpClient.PostAsync($"{_configuration["CommandService"]}api/c/Platforms", httpContent);
+
+            //! Short of if and else
+            //Console.WriteLine(response.IsSuccessStatusCode
+            //    ? "--> Sync Post to CommandService was ok ! <--"
+            //    : "--> Sync Post to CommandService was Not OK ! <--");
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("--> Sync Post to CommandService was ok ! <--");
+            }
+            else
+            {
+                Console.WriteLine("--> Sync Post to CommandService was Not OK ! <--");
+            }
+        }
     }
 }
