3615bbe458575a89781b63e03ac369453380b73c
itz-amethyst <pransermi@gmail.com>
2023-06-12 11:16:40 +0430

    feat: Added new webapi service

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+
+    }
+}
