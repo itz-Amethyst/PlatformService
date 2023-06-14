69405af17a274af1d023467c9038ee86ad01163d
itz-amethyst <pransermi@gmail.com>
2023-06-14 22:53:17 +0430

    feat: Added commandMapping

---
+++ CommandManagement.Infrastructure.EFCore/Mappings/CommandMapping.cs
@@ -0,0 +1,16 @@
+﻿using CommandManagement.Domain.Commands;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CommandManagement.Infrastructure.EFCore.Mappings
+{
+    public class CommandMapping : IEntityTypeConfiguration<Command>
+    {
+        public void Configure(EntityTypeBuilder<Command> builder)
+        {
+            builder.HasOne(x => x.Platform)
+                .WithMany(x => x.Commands)
+                .HasForeignKey(x => x.PlatformId);
+        }
+    }
+}
