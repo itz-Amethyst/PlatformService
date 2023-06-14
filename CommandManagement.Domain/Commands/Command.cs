61232d01ba55f372b08de83b0e302bdf6a790ca2
itz-amethyst <pransermi@gmail.com>
2023-06-14 22:39:52 +0430

    feat: Added Data Annotation

--- CommandManagement.Domain/Commands/Command.cs
+++ CommandManagement.Domain/Commands/Command.cs
@@ -1,15 +1,21 @@
 ﻿using CommandManagement.Domain.Platforms;
+using System.ComponentModel.DataAnnotations;
 
-namespace CommandManagement.Domain.Command
+namespace CommandManagement.Domain.Commands
 {
     public class Command
     {
+        [Key]
+        [Required]
         public int Id { get; private set; }
 
+        [Required]
         public string HowTo { get; private set; }
 
+        [Required]
         public string CommandLine { get; private set; }
 
+        [Required]
         public int PlatformId { get; private set; }
 
         public Platform Platform { get; private set; }
--- CommandManagement.Domain/Platforms/Platform.cs
+++ CommandManagement.Domain/Platforms/Platform.cs
@@ -1,12 +1,23 @@
-﻿namespace CommandManagement.Domain.Platforms
+﻿using System.ComponentModel.DataAnnotations;
+using CommandManagement.Domain.Commands;
+
+namespace CommandManagement.Domain.Platforms
 {
     public class Platform
     {
+        //? In here i use DataAnnotations you can use custom mappings either
+
+        [Key]
+        [Required]
         public int Id { get; private set; }
 
+        [Required]
         public int ExternalId { get; private set; }
 
+        [Required]
         public string Name { get; private set; }
+
+        public ICollection<Command> Commands { get; private set; } = new List<Command>();
 
         public Platform(int id, int externalId, string name)
         {
