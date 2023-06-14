ece4561e8fdf3bcde896ef49ac74d179f4313f0e
itz-amethyst <pransermi@gmail.com>
2023-06-14 22:53:26 +0430

    feat: Added platformMapping

---
+++ CommandManagement.Infrastructure.EFCore/Mappings/PlatformMapping.cs
@@ -0,0 +1,16 @@
+﻿using CommandManagement.Domain.Platforms;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CommandManagement.Infrastructure.EFCore.Mappings
+{
+    public class PlatformMapping : IEntityTypeConfiguration<Platform>
+    {
+        public void Configure(EntityTypeBuilder<Platform> builder)
+        {
+            builder.HasMany(x => x.Commands)
+                .WithOne(x => x.Platform!)
+                .HasForeignKey(x => x.PlatformId);
+        }
+    }
+}
