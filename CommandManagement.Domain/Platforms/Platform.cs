00356d4777a2d77b7386bf31ca29ea0a5a7317bc
itz-amethyst <pransermi@gmail.com>
2023-06-14 22:34:17 +0430

    feat: Added Command Platform domain model

---
+++ CommandManagement.Domain/Platforms/Platform.cs
@@ -0,0 +1,18 @@
+﻿namespace CommandManagement.Domain.Platforms
+{
+    public class Platform
+    {
+        public int Id { get; private set; }
+
+        public int ExternalId { get; private set; }
+
+        public string Name { get; private set; }
+
+        public Platform(int id, int externalId, string name)
+        {
+            Id = id;
+            ExternalId = externalId;
+            Name = name;
+        }
+    }
+}
