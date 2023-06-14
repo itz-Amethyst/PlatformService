9ea175a78d54b8afebb9b4ec4b0688823253eff4
itz-amethyst <pransermi@gmail.com>
2023-06-14 22:53:00 +0430

    feat: Added Command Context

---
+++ CommandManagement.Infrastructure.EFCore/Context/CommandContext.cs
@@ -0,0 +1,18 @@
+﻿using CommandManagement.Domain.Commands;
+using CommandManagement.Domain.Platforms;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandManagement.Infrastructure.EFCore.Context
+{
+    public class CommandContext: DbContext
+    {
+        public DbSet<Platform> Platforms { get; set; }
+
+        public DbSet<Command> Commands { get; set; }
+
+        public CommandContext(DbContextOptions<CommandContext> options) : base(options)
+        {
+            
+        }
+    }
+}
