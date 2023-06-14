628505970557f54313cb5a4e3e400b861d4ab604
itz-amethyst <pransermi@gmail.com>
2023-06-14 22:34:32 +0430

    feat: Added command Domain model

---
+++ CommandManagement.Domain/Command/Command.cs
@@ -0,0 +1,26 @@
+﻿using CommandManagement.Domain.Platforms;
+
+namespace CommandManagement.Domain.Command
+{
+    public class Command
+    {
+        public int Id { get; private set; }
+
+        public string HowTo { get; private set; }
+
+        public string CommandLine { get; private set; }
+
+        public int PlatformId { get; private set; }
+
+        public Platform Platform { get; private set; }
+
+        public Command(int id, string howTo, string commandLine, int platformId, Platform platform)
+        {
+            Id = id;
+            HowTo = howTo;
+            CommandLine = commandLine;
+            PlatformId = platformId;
+            Platform = platform;
+        }
+    }
+}
