ca452ef460741012b8fa4d643128c945c071fc1b
itz-amethyst <pransermi@gmail.com>
2023-06-15 19:47:01 +0430

    feat: Added CreateCommand

---
+++ CommandManagement.Application.Contracts/Command/CreateCommand.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CommandManagement.Application.Contracts.Command
+{
+    public class CreateCommand
+    {
+        public string HowTo { get; set; }
+
+        public string CommandLine { get; set; }
+    }
+}
