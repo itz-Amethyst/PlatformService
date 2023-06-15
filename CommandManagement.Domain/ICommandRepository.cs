5e4bbd88bf0d0495d63ada3c6b8f98fa293862a9
itz-amethyst <pransermi@gmail.com>
2023-06-15 19:47:37 +0430

    feat: Added CommandRepository

---
+++ CommandManagement.Domain/ICommandRepository.cs
@@ -0,0 +1,15 @@
+﻿using CommandManagement.Domain.Commands;
+
+namespace CommandManagement.Domain
+{
+    public interface ICommandRepository
+    {
+        //? Because its external i didn't create it as generic in Repository Framework 
+        // Commands
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+
+        Command GetCommand(int platformId , int commandId);
+
+        void CreateCommand(int id);
+    }
+}
