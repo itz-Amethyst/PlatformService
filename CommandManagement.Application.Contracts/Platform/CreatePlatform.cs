b768ce6c7add5c563664802778489f060d97ed66
itz-amethyst <pransermi@gmail.com>
2023-06-15 19:47:09 +0430

    feat: Added CreatePlatform

---
+++ CommandManagement.Application.Contracts/Platform/CreatePlatform.cs
@@ -0,0 +1,14 @@
+﻿
+namespace CommandManagement.Application.Contracts.Platform
+{
+    public class CreatePlatform
+    {
+        public int Id { get; set; }
+
+        public int ExternalId { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Command> Commands { get; set; } 
+    }
+}
