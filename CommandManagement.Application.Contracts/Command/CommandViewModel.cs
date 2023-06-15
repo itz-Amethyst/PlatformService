36a909c4728d086687b33ce8844d9363fb01005e
itz-amethyst <pransermi@gmail.com>
2023-06-15 19:46:41 +0430

    feat: Added CommandViewModel

---
+++ CommandManagement.Application.Contracts/Command/CommandViewModel.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CommandManagement.Application.Contracts.Command
+{
+    public class CommandViewModel
+    {
+        public int Id { get; set; }
+
+        public string HowTo { get; set; }
+
+        public string CommandLine { get; set; }
+
+        public int PlatformId { get; set; }
+
+    }
+}
