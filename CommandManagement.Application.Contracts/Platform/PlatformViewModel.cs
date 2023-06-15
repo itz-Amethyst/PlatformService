64e6fe38863081d816c8ca16cdc910c2bdaadc11
itz-amethyst <pransermi@gmail.com>
2023-06-15 19:47:19 +0430

    feat: Added PlatformViewModel

---
+++ CommandManagement.Application.Contracts/Platform/PlatformViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CommandManagement.Application.Contracts.Platform
+{
+    public class PlatformViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
