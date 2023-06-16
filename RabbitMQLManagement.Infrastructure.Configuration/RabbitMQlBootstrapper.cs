5d69beacc225d741b44f104f4af9e220c6452a6b
itz-amethyst <pransermi@gmail.com>
2023-06-16 21:12:02 +0430

    feat: Added RabbitMQlBootsrapper

---
+++ RabbitMQLManagement.Infrastructure.Configuration/RabbitMQlBootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RabbitMQLManagement.Domain;
+using RabbitMQLManagement.Infrastructure.Services.Repository.AsyncDataServices;
+
+namespace RabbitMQLManagement.Infrastructure.Configuration
+{
+    public class RabbitMQlBootstrapper
+    {
+        public static void Configure(IServiceCollection services)
+        {
+            #region RabbitMqlService
+
+            services.AddSingleton<IMessageBusClient, MessageBusClient>();
+
+            #endregion
+
+        }
+    }
+}
