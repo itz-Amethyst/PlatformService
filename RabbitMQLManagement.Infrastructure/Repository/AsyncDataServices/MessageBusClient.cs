3ecd0a91ad4e66298542306372d3f55f34aee735
itz-amethyst <pransermi@gmail.com>
2023-06-16 20:08:07 +0430

    feat: Added Basic Configurations of RabbitMq

--- RabbitMQLManagement.Infrastructure/Repository/AsyncDataServices/MessageBusClient.cs
+++ RabbitMQLManagement.Infrastructure/Repository/AsyncDataServices/MessageBusClient.cs
@@ -1,10 +1,41 @@
-﻿using RabbitMQLManagement.Application.Contracts;
+﻿using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using RabbitMQLManagement.Application.Contracts;
 using RabbitMQLManagement.Domain;
 
-namespace RabbitMQLManagement.Infrastructure.Repository.AsyncDataServices
+namespace RabbitMQLManagement.Infrastructure.Services.Repository.AsyncDataServices
 {
     public class MessageBusClient : IMessageBusClient
     {
+
+        private readonly IConfiguration _configuration;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public MessageBusClient(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var factory = new ConnectionFactory()
+            {
+                //HostName = _configuration["RabbitMQHost"]
+                HostName = _configuration.GetSection("RabbitMQHost").Value,
+                Port = int.Parse(_configuration.GetSection("RabbitMQPort").Value)
+            };
+
+            try
+            {
+                _connection = factory.CreateConnection();
+                _channel = _connection.CreateModel();
+
+                _channel.ExchangeDeclare(exchange: "trigger" , type: ExchangeType.Fanout);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"--> Could Not Connect to the Message Bus : {e.Message}");
+                throw;
+            }
+        }
+
         public void PublishNewPlatform(PlatformPublishedViewModel platformPublishedViewModel)
         {
             throw new NotImplementedException();
