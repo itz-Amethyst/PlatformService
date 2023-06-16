4d49c230bc03e2e4d73c503c2d420f075addcebc
itz-amethyst <pransermi@gmail.com>
2023-06-16 19:58:53 +0430

    feat: Added Rabbit Published View Model

---
+++ RabbitMQLManagement.Application.Contracts/PlatformPublishedViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RabbitMQLManagement.Application.Contracts
+{
+    public class PlatformPublishedViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Event { get; set; }
+    }
+}
