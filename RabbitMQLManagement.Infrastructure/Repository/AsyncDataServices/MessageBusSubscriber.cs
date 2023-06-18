defeca020cfcd273bac5e3af3fdc9a1b26eb2540
itz-amethyst <pransermi@gmail.com>
2023-06-18 15:06:36 +0430

    feat: Added basic of MessageBusSubscriber

---
+++ RabbitMQLManagement.Infrastructure/Repository/AsyncDataServices/MessageBusSubscriber.cs
@@ -0,0 +1,23 @@
+﻿using _0_Framework.Application.EventProcessing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace RabbitMQLManagement.Infrastructure.Services.Repository.AsyncDataServices
+{
+    public class MessageBusSubscriber : BackgroundService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IEventProcessor _eventProcessor;
+
+        public MessageBusSubscriber(IConfiguration configuration, IEventProcessor eventProcessor)
+        {
+            _configuration = configuration;
+            _eventProcessor = eventProcessor;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
