4fa413487676d6120ad0513ee6394f33f3fd362a
itz-amethyst <pransermi@gmail.com>
2023-06-18 15:24:18 +0430

    feat: Added Common methods in MessageBus

---
+++ RabbitMQLManagement.Infrastructure/Repository/AsyncDataServices/Common/IMessageBus.cs
@@ -0,0 +1,11 @@
+﻿using RabbitMQ.Client;
+
+namespace RabbitMQLManagement.Infrastructure.Services.Repository.AsyncDataServices.Common
+{
+    public interface IMessageBus
+    {
+        void Dispose();
+
+        void RabbitMQ_ConnectionShutDown(object sender, ShutdownEventArgs e);
+    }
+}
