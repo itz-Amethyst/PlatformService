40170cdbbf51d1c741609ed7403be5a3af1e80dd
itz-amethyst <pransermi@gmail.com>
2023-06-18 13:23:06 +0430

    feat: Fixed create platform inside event processor because of db tracker

---
+++ CommandManagement.Domain/Platforms/IPlatformRepository.cs
@@ -0,0 +1,9 @@
+﻿using _0_Framework.Domain;
+
+namespace CommandManagement.Domain.Platforms
+{
+    public interface IPlatformRepository : IRepository<int , Platform>
+    {
+
+    }
+}
---
+++ CommandManagement.Infrastructure.EFCore/Repository/PlatformRepository.cs
@@ -0,0 +1,16 @@
+﻿using _0_Framework.Infrastructure;
+using CommandManagement.Domain.Platforms;
+using CommandManagement.Infrastructure.EFCore.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandManagement.Infrastructure.EFCore.Repository
+{
+    public class PlatformRepository : RepositoryBase<int , Platform> , IPlatformRepository
+    {
+        private readonly CommandContext _commandContext;
+        public PlatformRepository(CommandContext commandContext) : base(commandContext)
+        {
+            _commandContext = commandContext;
+        }
+    }
+}
