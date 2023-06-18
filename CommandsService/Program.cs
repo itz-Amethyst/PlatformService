62de69c024fa289bc102addbbe3c01a967cfef78
itz-amethyst <pransermi@gmail.com>
2023-06-18 15:28:50 +0430

    feat: Wired up in command program subscriber event

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,10 +1,13 @@
 using CommandManagement.Infrastructure.Configuration;
+using RabbitMQLManagement.Infrastructure.Services.Repository.AsyncDataServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+//? IDK it should implement in here
+builder.Services.AddHostedService<MessageBusSubscriber>();
 CommandManagementBootstrapper.Configure(builder.Services);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
