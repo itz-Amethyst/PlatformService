ddb899abbd39ed8ec65ba05076c6405077c5b804
itz-amethyst <pransermi@gmail.com>
2023-06-20 13:21:03 +0430

    feat: PlatformService Grpc

--- PlatformManagement.Infrastructure.Services/SyncDataServices/Grpc/GrpcPlatformService.cs
+++ PlatformManagement.Infrastructure.Services/SyncDataServices/Grpc/GrpcPlatformService.cs
@@ -1,9 +1,32 @@
-﻿using PlatformService;
+﻿using AutoMapper;
+using Grpc.Core;
+using PlatformManagement.Domain;
+using PlatformService;
 
 namespace PlatformManagement.Infrastructure.Services.SyncDataServices.Grpc
 {
     public class GrpcPlatformService : GrpcPlatform.GrpcPlatformBase
     {
+        private readonly IPlatformRepository _platformRepository;
+        private readonly IMapper _mapper;
+        public GrpcPlatformService(IPlatformRepository platformRepository, IMapper mapper)
+        {
+            _platformRepository = platformRepository;
+            _mapper = mapper;
+        }
 
+        public override Task<PlatformResponse> GetAllPlatforms(GetAllRequest request , ServerCallContext context)
+        {
+            var response = new PlatformResponse();
+
+            var platforms = _platformRepository.GetAllPlatforms();
+
+            foreach (var plat in platforms)
+            {
+                response.Platform.Add(_mapper.Map<GrpcPlatformModel>(plat));
+            }
+
+            return Task.FromResult(response);
+        }
     }
 }
