b462dc1a74750ad121983b68e0e6b77ed6ae038a
itz-amethyst <pransermi@gmail.com>
2023-06-20 13:18:20 +0430

    feat: added Grpc Platform Config

--- PlatformService/AutoMapper/PlatformsProfile.cs
+++ PlatformService/AutoMapper/PlatformsProfile.cs
@@ -12,6 +12,10 @@ namespace PlatformService.AutoMapper
             CreateMap<Platform, PlatformViewModel>();
             CreateMap<CreatePlatform, Platform>();
             CreateMap<PlatformViewModel, PlatformPublishedViewModel>();
+            CreateMap<Platform, GrpcPlatformModel>()
+                .ForMember(x => x.PlatformId, z => z.MapFrom(src => src.Id))
+                .ForMember(x => x.Name, z => z.MapFrom(src => src.Name))
+                .ForMember(x => x.Publisher, z => z.MapFrom(src => src.Publisher));
         }
     }
 }
